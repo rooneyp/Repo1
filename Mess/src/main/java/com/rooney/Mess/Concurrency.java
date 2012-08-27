d640b2222bdb80cb021fcde3e3516d8e6882d9c9
rooneyp1976 <rooneyp1976@yahoo.com>
2012-08-27 13:53:55 +0100

    1

--- Mess/src/main/java/com/rooney/Mess/Concurrency.java
+++ Mess/src/main/java/com/rooney/Mess/Concurrency.java
@@ -1,24 +1,76 @@
 package com.rooney.Mess;
 
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Concurrency {
 
-	/**
-	 * @param args
-	 * @throws Exception 
-	 */
 	public static void main(String[] args) throws Exception {
-		basicBlockingQ();
-
+        new Concurrency().reentrantReadWriteLock();
 	}
 
-	private static void basicBlockingQ() throws InterruptedException {
-		BlockingQueue<String> bq = new ArrayBlockingQueue<String>(20);
-		
-		bq.put("1");
-		System.out.println(bq.remove());
-	}
+    private void reentrantReadWriteLock() throws Exception {
+        final ReadWriteLock rwl = new ReentrantReadWriteLock();
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        runThread(LockType.READ, rwl, executor, 3000, 0);
+        runThread(LockType.READ, rwl, executor, 3000, 0);
+        Thread.sleep(500); // want to delay the write lock aquistion as it will block read lock acquisitions
+        runThread(LockType.WRITE, rwl, executor, 3000, 1000);
+    }
+
+    public enum LockType {
+        READ, WRITE
+    }
+
+    public void runThread(final LockType lockType, final ReadWriteLock rwl, ExecutorService executor,
+            final int sleepTimeMills, final int timeToWaitOnLockMillis) {
+        executor.execute(new Runnable() {
+            public void run() {
+                log(Thread.currentThread() + " acquiring Lock " + lockType);
+
+                Lock lock = null;
+                switch (lockType) {
+                    case READ:
+                        lock = rwl.readLock();
+                        break;
+
+                    case WRITE:
+                        lock = rwl.writeLock();
+                        break;
+                }
+
+                boolean lockAcquired = false;
+                try {
+                    try {
+                        lockAcquired = lock.tryLock(timeToWaitOnLockMillis, TimeUnit.MILLISECONDS);
+                        log(Thread.currentThread() + " lockAcquired=" + lockAcquired + ", type="
+                                + lockType);
+                        log(Thread.currentThread() + " sleeping for " + sleepTimeMills);
+                        Thread.sleep(sleepTimeMills);
+                    } catch (InterruptedException e) {
+                        log(Thread.currentThread() + " interrupted");
+                    }
+                } finally {
+                    if (lock != null && lockAcquired) {
+                        lock.unlock();
+                    }
+                    log(Thread.currentThread() + " finished and unlocked " + lockType);
+                }
+            }
+
+            private void log(String string) {
+                System.out.println(new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()) + " " + string);
+
+            }
+        });
+    }
 
 }
