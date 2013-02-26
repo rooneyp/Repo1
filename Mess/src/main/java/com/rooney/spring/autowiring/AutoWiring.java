4ad684daee073a4d26d9245cddfa2dcb77f84bbb
rooneyp1976 <rooneyp1976@yahoo.com>
2013-02-26 11:38:09 +0000

    a

--- Mess/src/main/java/com/rooney/spring/autowiring/AutoWiring.java
+++ Mess/src/main/java/com/rooney/spring/autowiring/AutoWiring.java
@@ -12,9 +12,14 @@ public class AutoWiring {
      * @param args
      */
     public static void main(String[] args) {
+//        autowiring();
+        chooseBeanImplUsingLabelInConfigOrSysProp();
+    }
+
+    public static void autowiring() {
         ApplicationContext appCtx = new ClassPathXmlApplicationContext("classpath:**/AutoWiring-context.xml");
 //        System.out.println(appCtx.getBean("scheduler"));
-//        System.out.println(appCtx.getBean("test"));
+//        System.out.println(appCtx.getBean("test"));        
         
         MyBean bean = new MyBean();
         System.out.println(bean);
@@ -26,6 +31,12 @@ public class AutoWiring {
         System.out.println(bean);
     }
 
+    public static void chooseBeanImplUsingLabelInConfigOrSysProp() {
+        System.setProperty("oam.choice", "remote"); //friendly value 'remote' for meaningful property 'oam.choice'
+        ApplicationContext appCtx = new ClassPathXmlApplicationContext("classpath:**/AutoWiring-context.xml");
+        System.out.println(appCtx.getBean(MyInterface.class));
+    }
+    
     
     public static class MyBean {
         @Autowired TaskScheduler scheduler;
@@ -34,7 +45,16 @@ public class AutoWiring {
         public String toString() {
             return super.toString() + " test: " + test + " sch: " +scheduler;
         }
-        
-        
     }
+    
+    public static interface MyInterface {
+        public String toString();
+    }    
+    
+    public static class MyImpl1 implements MyInterface{
+    }    
+    
+    public static class MyImpl2 implements MyInterface{
+    }    
+    
 }
