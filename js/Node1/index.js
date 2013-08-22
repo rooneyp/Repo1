83ea360a71138b37c27b18aae6c1b3625e5c307c
Paul Rooney <rooneyp1976@yahoo.com>
2013-08-22 08:02:33 +0100

    a

---
+++ js/Node1/index.js
@@ -0,0 +1,10 @@
+/**
+ * New node file
+ */
+console.log("Starting index.js");
+
+var server = require("./server");
+server.start();
+console.log("Started Server");
+
+console.log("Finishing index.js");
