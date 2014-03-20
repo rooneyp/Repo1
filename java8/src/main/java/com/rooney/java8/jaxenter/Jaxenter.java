b9dfbe7b0f2f5f3d594b86366b5e2076b442cb87
rooneyp1976 <rooneyp1976@yahoo.com>
2014-03-20 08:13:54 +0000

    a

--- java8/src/main/java/com/rooney/java8/jaxenter/Jaxenter.java
+++ java8/src/main/java/com/rooney/java8/jaxenter/Jaxenter.java
@@ -1,10 +1,33 @@
 package com.rooney.java8.jaxenter;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class Jaxenter {
 
 	public static void main(String... args) {
 	    listing3();
+
+		listing4();
 	  }
+
+	private static void listing4() {
+		Comparator<String> c = (String lhs, String rhs) -> lhs.compareTo(rhs);
+		int result = c.compare("Hello", "World");
+		System.out.println("result is " + result);
+		
+		List<String> strings = Arrays.asList("A", "C", "B") ;
+		strings.sort((lhs, rhs) -> lhs.compareTo(rhs)); //minimal impl for finctional interface
+		System.out.println(strings);
+
+		strings.sort((lhs, rhs) -> {
+			System.out.println("comparing"); 
+			return lhs.compareTo(rhs);
+		}); //extra logic impl for finctional interface
+		System.out.println(strings);
+		
+	}
 
 	private static void listing3() {
 		Runnable r2 = () -> System.out.println("Howdy, world!");
