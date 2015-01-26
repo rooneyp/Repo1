2f23262af6a87ba04cf0f1ab71691c076457a532
Paul Rooney <rooneyp1976@yahoo.com>
2015-01-26 17:41:57 +0000

    x

---
+++ java8/j8ia/mod1/src/com/rooney/java8/j8ia.java
@@ -0,0 +1,11 @@
+package com.rooney.java8;
+
+/**
+ * Created by paul on 31/10/14.
+ * https://www.jetbrains.com/idea/webhelp/adding-modules-to-a-project.html
+ */
+public class j8ia {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
