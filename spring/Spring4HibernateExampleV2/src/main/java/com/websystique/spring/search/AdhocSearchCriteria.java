4f4968b78be38cb6b99770c710bbc97a85635baa
Paul Rooney <rooneyp1976@yahoo.com>
2015-07-22 21:32:55 +0100

    x

--- spring/Spring4HibernateExampleV2/src/main/java/com/websystique/spring/search/AdhocSearchCriteria.java
+++ spring/Spring4HibernateExampleV2/src/main/java/com/websystique/spring/search/AdhocSearchCriteria.java
@@ -1,5 +1,10 @@
 package com.websystique.spring.search;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,26 +27,32 @@ public class AdhocSearchCriteria {
     protected Set<String> dateFields = Sets.newHashSet();
     
     
-    public AdhocSearchCriteria(Map<String, String> requestMap) {
-        // TODO Auto-generated constructor stub
+    public AdhocSearchCriteria(Map<String, String> requestMap, List<Map<String, Object>> filters) {
+        this.filters = filters;
+
     }
 
     
     
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String requestParams = "["
                 + "{\"operator\":\"like\",\"value\":\"pictures\",\"property\":\"messageText\"}"
                 + ",{\"operator\":\"gt\",\"value\":4,\"property\":\"priority\"}"
                 + ",{\"operator\":\"lt\",\"value\":1152399600000,\"property\":\"added\"}"
-                + "]";        
-        
+                + "]";
+
+        TypeReference<List<HashMap<String, Object>>> typeRef = new TypeReference<List<HashMap<String, Object>>>() {
+        };
+        List<Map<String, Object>> filters = new ObjectMapper().readValue(requestParams, typeRef);
+        System.out.println("filters " + filters);
+
         Map<String, String> requestMap = new HashMap<String, String>();
         
         requestMap.put("sort", "mySortField");
         requestMap.put("filter", requestParams);
         
-        AdhocSearchCriteria criteria = new AdhocSearchCriteria(requestMap);
+        AdhocSearchCriteria criteria = new AdhocSearchCriteria(requestMap, filters);
         
         //test: getquery
         //test: getTotalCount
