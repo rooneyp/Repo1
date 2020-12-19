47fbd95bf74741129c0a6995994da1c4881637ff
rooneyp <rooneyp@gmail.com>
2020-12-19 10:01:42 +0000

    initial

---
+++ kong/src/main/java/com/rooney/kong/KongApplication.java
@@ -0,0 +1,13 @@
+package com.rooney.kong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongApplication.class, args);
+	}
+
+}
